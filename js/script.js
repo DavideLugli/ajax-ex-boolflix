7f4c5ff56918a8b44881f7213149ab8a1d13985b
Davide Lugli <luglidavide.web@gmail.com>
2020-02-06 11:38:17 +0100

    Api non dinamica WORKING

--- js/script.js
+++ js/script.js
@@ -4,24 +4,41 @@ $(document).ready(function() {
 
   // mia chiave api
   // de1660ae74e359f522a07d42f19001a0
+  // var userSearch = $('#movie-search').val();
   $.ajax({
     url: 'https://api.themoviedb.org/3/search/movie',
     method: 'GET',
     data: {
       api_key: 'de1660ae74e359f522a07d42f19001a0',
       language: 'it-It',
-      query: 'Ritorno al futuro',
+      query: 'Ritorno al Futuro',
     },
     success: function(data) {
-      console.log(data.results);
+      var moviesFound = data.results;
+      printMovies(moviesFound);
+
     },
     error: function(request, state, errors) {
       console.log(errors);
     }
   })
-  // handlebars
-  // var source = document.getElementById("entry-template").innerHTML;
-  // var template = Handlebars.compile(source);
-  // var context = { title: "My New Post", body: "This is my first post!" };
-  // var html = template(context);
-})
+});
+
+
+// FUNZIONI
+function printMovies(movies) {
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < movies.length; i++) {
+    var thisMovie = movies[i];
+    var context = {
+      title: 'Titolo: ' + thisMovie.title,
+      original_title: 'Titolo originale: ' + thisMovie.original_title,
+      original_language: 'Lingua Originale: ' + thisMovie.original_language,
+      vote_average: 'Voto Medio Utenti: ' + thisMovie.vote_average
+    };
+    var html = template(context);
+    $('#movie-list').append(html);
+  }
+
+}
